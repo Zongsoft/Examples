152ebe7f6dcc04f0b509453162858ebfe0838452
Popeye Zhong <zongsoft@gmail.com>
2014-09-03 17:35:44 +0800

    将项目版本的.NET框架版本由4.0升级到4.5

---
+++ Zongsoft.Examples.Data.Metadata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Zongsoft.Data;
+using Zongsoft.Data.Metadata;
+
+namespace Zongsoft.Examples.Data.Metadata
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var resolver = new MetadataFileResolver();
+			var metadataFile = resolver.Resolve(@"E:\Zongsoft\Zongsoft.Data\Readme.xml");
+		}
+	}
+}
